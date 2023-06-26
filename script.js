b526dfb6b0684896bc9213ff715ae4b80248c655
Tobias Konzok <konzok.tobias@gmail.com>
2023-06-26 09:55:53 +0200

    Code calc operation functions

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log('Hello World!')
+const add = (a,b) => a + b;
+const subtract = (a,b) => a - b;
+const multiply = (a,b) => a * b;
+const divide = (a,b) => a / b;
+
+let num1 = 0;
+let num2 = 0;
+let operator = "";
+
+function operation (operator, num1, num2) {
+    if (operator === "add") {
+        return add(num1, num2);
+    } else if (operator === "subtract") {
+        return subtract(num1, num2);
+    } else if (operator === "multiply") {
+        return multiply(num1, num2);
+    } else {
+        return divide(num1, num2);
+    }
+}
